48e90dbd27172901a7a2a089d5fdc92882b509b2
Dalen W Brauner <DalenWBrauner@gmail.com>
2014-03-18 13:27:05 -0400

    Update.py is ALMOST OPERATIONAL!
    I currently have only developed functionality for First and Repeat
    Births from Florida Charts, but I'm having some other issues that I
    don't want to lose before I move on.
    Namely, that I can't tell if the database is actually receiving the
    information my code is convinced it is.

--- Update.py
+++ Update.py
@@ -1,74 +1,37 @@
 """
 Update.py
-This file is designed to retrieve any and all csv files in
-the given directory and read them as new entries to the database.
+This file is designed to pull certain csv files in the given
+directory and upload them as new entries to the database.
 
 Written by Dalen W. Brauner
 Last Maintained: 03/17/2014
 """
+from Gimmie import Check_For_Updates, Prep_For_The_Database
+import sqlite3
+CONNECTION = sqlite3.connect('SQLite.db')
+SQL = CONNECTION.cursor()
 
 #
 ##
-### Assist Functions
+### Data Upload Functions
+def Upload(where_to,data):
+    if   where_to == "Florida Charts_First Births.csv" \
+       or where_to == "Florida Charts_Repeat Births.csv":
+        SQL.execute("BEGIN;")
+        SQL.executemany("""INSERT INTO births VALUES (?,?,?,?,?,?,?);""", data)
+##        SQL.executemany("""
+##                        BEGIN;
+##                        INSERT INTO births VALUES (?,?,?,?,?,?,?);
+##                        COMMIT;""", data)
+##        SQL.execute("COMMIT;")
+        CONNECTION.commit()
+        print "SUCCESS QUESTION MARK?"
+        SQL.execute("""SELECT * FROM births WHERE county='Washington' AND Mothers_Age=42""")
+        print SQL.fetchall()
 
-def Check_For_Updates(l1st):
-    """
-    Checks each filename in the given list for a csv file.
-    Returns a Dictionary of each discovered csv as the Value
-    and the filename as the Key.
-    """
-    
-    DATA = {}
-    for filename in l1st:
-        result = Retrieve(filename)
-        if result:
-            DATA[filename] = result
-    
-    return DATA
-
-def Retrieve(filename):
-    """
-    Returns a List of the rows in the given csv.
-    Returns None if the file does not exist.
-    """
-
-    # Makes the attempt
-    try:
-        # Retrieves file
-        f1le = open(filename,"r")
-        raw_data = f1le.read().split('\n')
-        f1le.close()
-
-    # Returns None if the file DNE
-    except IOError:
-        return None
-
-    # Formats from string to compound list
-    fixed_data = raw_data[:]
-    for d in xrange(len(raw_data)):
-        fixed_data[d] = raw_data[d].split(',')
-    
-    return fixed_data
-
-def Prep_For_The_Database(how_so,csv):
-    if   how_so == "Florida Charts_First Births.csv":
-        print "FORMATTING '" + how_so + "' INCOMPLETE"
-        
-    elif how_so == "Florida Charts_Repeat Births.csv":
-        print "FORMATTING '" + how_so + "' INCOMPLETE"
-        
-    elif how_so == "Florida Charts_Diseases.csv":
-        print "FORMATTING '" + how_so + "' INCOMPLETE"
-        
-    elif how_so == "Florida Health_Births.csv":
-        print "FORMATTING '" + how_so + "' INCOMPLETE"
-        
-    elif how_so == "Florida Health_Diseases.csv":
-        print "FORMATTING '" + how_so + "' INCOMPLETE"
-        
     else:
-        ERR = "Hey, I'm not trained to format '"+how_so+"'!"
-        raise Exception(ERR)
+        print "No good."
+        
 
 #
 ##
@@ -83,19 +46,12 @@ def main():
     updates = Check_For_Updates(UPDATE_THESE)
     if len(updates) == 0:
         print "No updates!"
+        quit()  # If there aren't any updates, don't waste your time!
     for key in updates:
         updates[key] = Prep_For_The_Database(key,updates[key])
+        Upload(key,updates[key])
 
-#
-##
-### Debug Functions
+    CONNECTION.close()
 
-def _debug_Print_The_Data(csv):
-    """It ain't pretty, but"""
-    for row in fixed_data:
-        output = '| '
-        for cell in row:
-            output += cell + '\t| '
-        print output,'\n'
 
-if __name__ == "__main__":  main()
+if __name__ == "__main__":main()
