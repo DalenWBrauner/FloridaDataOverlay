5bdd6c8e7763086012f06506bd8c68a323ad7353
beschofield <beschofield@users.noreply.github.com>
2014-04-22 14:36:08 -0400

    accesses the database

--- Starting_Over/Florida_Data_Overlay/Overlay/views.py
+++ Starting_Over/Florida_Data_Overlay/Overlay/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def basic(request):
+    #Births.objects.all()
+    #html = "<html><body>INSERT THING HERE</body></html>"
+
+    my_list=list(Births.objects.all())
+    render basic_temp.html, my_list
