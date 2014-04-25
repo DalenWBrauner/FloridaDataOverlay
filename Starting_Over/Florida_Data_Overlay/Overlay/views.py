faf7b1055f2017f15321cc18b0d038b6f67b6a27
beschofield <beschofield@users.noreply.github.com>
2014-04-25 11:31:29 -0400

    Update views.py

--- Starting_Over/Florida_Data_Overlay/Overlay/views.py
+++ Starting_Over/Florida_Data_Overlay/Overlay/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import RequestContext, loader
+
+from Overlay.models import Births
+
+#def basic(request):
+#    my_list=list(Births.objects.all())
+#    render basic_temp.html, my_list
 
 def basic(request):
-    #Births.objects.all()
-    #html = "<html><body>INSERT THING HERE</body></html>"
-
-    my_list=list(Births.objects.all())
-    render basic_temp.html, my_list
+#    list = Births.objects.order_by('-County')[:5]
+#    my_list = (Births.objects.all().order_by('-county'))[:5]
+    my_list=Births.objects.all()
+    template=loader.get_template('base.html')
+    context=RequestContext(request, {'my_list': my_list})
+    
+    return HttpResponse(template.render(context))
