c13d63cb20af376742f6f38f89ec97c7e4a44439
beschofield <beschofield@users.noreply.github.com>
2014-04-28 19:13:31 -0400

    Update views.py

--- Starting_Over/Florida_Data_Overlay/Overlay/views.py
+++ Starting_Over/Florida_Data_Overlay/Overlay/views.py
@@ -4,28 +4,52 @@ from django.template import RequestContext, loader
 
 from Overlay.models import Births
 
-#def basic(request):
-#    my_list=list(Births.objects.all())
-#    render basic_temp.html, my_list
-
 def basic(request):
-#    list = Births.objects.order_by('-County')[:5]
-#    my_list = (Births.objects.all().order_by('-county'))[:5]
     my_list=Births.objects.values('county').distinct()
+    
     template=loader.get_template('base.html')
     context=RequestContext(request, {'my_list': my_list})
     
     return HttpResponse(template.render(context))
 
 def year(request, cnty):
+    my_list = Births.objects.all().filter(county__exact = cnty)
+    my_list = my_list.order_by('-year').values('year').distinct()
     
-    my_list = Births.objects.all().filter(county__exact = cnty).order_by('-year').values('year').distinct()
     template = loader.get_template('years.html')
-    context = RequestContext(request, {'county': cnty, 'my_list': my_list})
+    context = RequestContext(request, {'county': cnty,
+                                       'my_list': my_list})
     
     return HttpResponse(template.render(context))
 
 def att(request, cnty, yr):
-    html='<html><body>derp</body></html>'
-    return HttpResponse(html)
+    obj = Births.objects.get(id=1)
+    names = obj.get_names()
+    fields = obj.get_fields()
+    d = []
 
+    for i in range(0, len(names)):
+        d.append([names[i], fields[i]])
+    
+    template = loader.get_template('attribute.html')
+    context = RequestContext(request, {'county': cnty,
+                                       'year': yr,
+                                       'names': names,
+                                       'fields': fields,
+                                       'dict': d})
+    
+    return HttpResponse(template.render(context))
+
+def table(request, cnty, yr, fld):
+    my_list = Births.objects.all().filter(county__exact = cnty)
+    my_list = my_list.filter(year__exact = yr)
+    data = my_list.values(fld)
+    
+    template = loader.get_template('table.html')
+    context = RequestContext(request, {'county': cnty,
+                                       'year': yr,
+                                       'field': fld,
+                                       'data': data,
+                                       'my_list': my_list})
+    
+    return HttpResponse(template.render(context))
