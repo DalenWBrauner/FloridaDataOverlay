ebc2a9520f666d4b001f89735272e102f619a7a6
beschofield <beschofield@users.noreply.github.com>
2014-04-28 19:13:07 -0400

    Update urls.py

--- Starting_Over/Florida_Data_Overlay/Overlay/urls.py
+++ Starting_Over/Florida_Data_Overlay/Overlay/urls.py
@@ -2,7 +2,19 @@ from django.conf.urls import url
 from Overlay import views
 
 urlpatterns = [
-    url(r'^$', views.basic, name='basic'),
-    url(r'^(?P<cnty>[^0-9]+)/$', views.year, name='years'),
-    url(r'^(?P<cnty>[^0-9]+)/(?P<yr>[0-9]{4})/$', views.att, name='attribute'),
+    url(r'^$',
+        views.basic,
+        name='basic'),
+    
+    url(r'^(?P<cnty>[^0-9]+)/$',
+        views.year,
+        name='years'),
+    
+    url(r'^(?P<cnty>[^0-9]+)/(?P<yr>[0-9]{4})/$',
+        views.att,
+        name='attribute'),
+
+    url(r'^(?P<cnty>[^0-9]+)/(?P<yr>[0-9]{4})/(?P<fld>[^0-9]+)/$',
+        views.table,
+        name='table'),
 ]
