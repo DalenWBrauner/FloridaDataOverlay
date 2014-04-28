7686c26c6dfcbe8f2510681740a12cd42891c104
beschofield <beschofield@users.noreply.github.com>
2014-04-28 16:27:39 -0400

    Update urls.py

--- Starting_Over/Florida_Data_Overlay/Overlay/urls.py
+++ Starting_Over/Florida_Data_Overlay/Overlay/urls.py
@@ -3,5 +3,6 @@ from Overlay import views
 
 urlpatterns = [
     url(r'^$', views.basic, name='basic'),
-    url(r'^(?P<county>[^0-9]+)/$', views.year, name='years'),
+    url(r'^(?P<cnty>[^0-9]+)/$', views.year, name='years'),
+    url(r'^(?P<cnty>[^0-9]+)/(?P<yr>[0-9]{4})/$', views.att, name='attribute'),
 ]
