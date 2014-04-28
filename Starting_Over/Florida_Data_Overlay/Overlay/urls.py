62ed0e0ab27b4a1d33c39485916cc3715880bcc1
beschofield <beschofield@users.noreply.github.com>
2014-04-28 15:46:35 -0400

    Update urls.py

--- Starting_Over/Florida_Data_Overlay/Overlay/urls.py
+++ Starting_Over/Florida_Data_Overlay/Overlay/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from Overlay import views
 
 urlpatterns = [
-    url(r'^$', views.basic, name='basic')
+    url(r'^$', views.basic, name='basic'),
+    url(r'^(?P<county>[^0-9]+)/$', views.year, name='years'),
 ]
